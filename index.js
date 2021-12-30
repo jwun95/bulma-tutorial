950cc083a45175f50e25f00f396fce1a63fdcc05
jwun95 <jwun95@naver.com>
2021-12-31 00:06:53 +0900

    13

--- index.js
+++ index.js
@@ -25,3 +25,16 @@ tabs.forEach((tab) => {
         })
     })
 })
+
+// modal
+const signupButton = document.querySelector('#signup');
+const modalBg = document.querySelector('.modal-backGround');
+const modal = document.querySelector('.modal');
+
+signupButton.addEventListener('click', () => {
+    modal.classList.add('is-active');
+});
+
+modalBg.addEventListener('click', () => {
+    modal.classList.remove('is-active');
+})
